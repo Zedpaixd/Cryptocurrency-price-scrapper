6365f0bd9fe2233ff5dfe5599609276d3b9ef502
Zedpai <70021545+Zedpaixd@users.noreply.github.com>
2020-09-20 03:52:17 +0300

    Add files via upload

---
+++ Scrapper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+from Cryptocurrency_tracker import amount
+
+Website = "https://cryptoprices.com/"
+res = requests.get(Website)
+res1 = res.text
+WebsiteData = BeautifulSoup(res1,'lxml')
+
+CoinName = WebsiteData.find_all('div',class_="coin-name")
+__CoinPrice__ = WebsiteData.find_all('tr')
+CoinPrice=__CoinPrice__[2].find_all('span')
+
+Prices = []
+
+for i in range (0,int(amount)):
+    CoinPrice=__CoinPrice__[i+1].find_all('span')
+    Prices.append(CoinPrice[1])
+    
+    print(Prices[i],CoinName[i])
+
